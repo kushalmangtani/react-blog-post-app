26e8bcb6248050f44c9cb16b326e2470450095b2
Kushal Mangtani <kushalmangtani@Kushals-MacBook-Pro.local>
2024-09-26 17:49:16 -0700

    api response fetch and render

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-class App extends Component{
-
-  constructor(props){
-    super(props)
+class App extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       posts: []
     }
   }
 
-  // Rendered once on mount
-  componentDidMount(){
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then(response => response.json)
-.then(json => {
-  this.setState({ posts: json})
-})
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then(response => response.json())
+    .then(json => this.setState({ posts: json }))
   }
 
-  render(){
-    return(
-    <div className='container'>
-      <div class="jumbotran">
-        <h1> Blog Post! </h1>
-      </div>
-      <div className='card'> 
-        <div className='card-header'>
-          Featured
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="container">
+        <div class="jumbotron">
+          <h1 class="display-4">Blog posts</h1>
         </div>
-        <div className='card-body'>
-        <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+        {posts.map((post) => (
+          <div className="card" key={post.id}>
+            <div className="card-header">
+              #{post.id} {post.title}
+            </div>
+            <div className="card-body">
+              <p className="card-text">{post.body}</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     );
   }
 }
-
 export default App;
