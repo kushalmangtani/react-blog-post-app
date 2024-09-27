46f95ac18045cb3c4600ef214470919458426f7c
Kushal Mangtani <kushalmangtani@Kushals-MacBook-Pro.local>
2024-09-26 17:24:46 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  // Rendered once on mount
+  componentDidMount(){
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json)
+.then(json => {
+  this.setState({ posts: json})
+})
+  }
+
+  render(){
+    return(
+    <div className='container'>
+      <div class="jumbotran">
+        <h1> Blog Post! </h1>
+      </div>
+      <div className='card'> 
+        <div className='card-header'>
+          Featured
+        </div>
+        <div className='card-body'>
+        <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
